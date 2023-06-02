f2fd15e8c8ebf0cb3165c80f9d46dfc2e46386be
cs1986 <132207568+cs1986@users.noreply.github.com>
2023-06-02 07:29:13 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,41 @@
+from sklearn.datasets import load_iris
+from sklearn import preprocessing
+import pandas as pd
+data=pd.read_csv("iris.csv")
+print(data)
+#isnull() function
+print(data.isnull())
+#is there any missing value along each column
+print(data.isnull().any())
+# count of missing values across each column using isna() and isnull()
+print(data.isnull().sum().sum())
+#count row wise missing value using isnull()
+print(data.isnull().sum(axis = 1))
+#count Column wise missing value using isnull()
+print(data.isnull().sum())
+#b
+print(data.isna().sum())
+#count of missing values of a specific column
+print(data.SepalLengthCm.isnull().sum())
+#groupby count of missing values of a column.
+print(data.groupby(['SepalLengthCm'])['PetalWidthCm'].apply(lambda x:
+x.isnull().sum()))
+#change data type
+data['petal lenth (cm)']=data['petal lenth (cm)'].astype("int")
+data.dtypes
+#Panda functions for Data Formatting and Normalization
+iris = load_iris()
+#create a pandas DataFrame with the iris data
+data = pd.DataFrame(iris.data,
+columns=iris.feature_names)
+#load iris dataset
+print(data.head())
+
+#x=data[['score']].values.astype(float)
+
+#unique values for species column
+print(data['sepal width (cm)'].unique())
+# one_hot_data_encoding
+one_hot_data = pd.get_dummies(data, prefix="sepal width (cm)",
+columns=['sepal width (cm)'], drop_first=False)
+print(one_hot_data)
---
+++ 2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+df=pd.read_csv('StudentsPerformance.csv')
+print(df)
+print(df.isnull())
+series=pd.isnull(df['Math_Score'])
+print(df[series])
+print(df.notnull())
+ndf=df
+col_drop=ndf.dropna(axis=1)
+print(col_drop)
+row_drop=ndf.dropna(axis=0,how='any')
+print(row_drop)
+col=['Math_Score','Reading_Score','Writing_Score','Placement_Score']
+df.boxplot(col)
+print(np.where(df['Math_Score']>90))
+print(np.where(df['Reading_Score']<25))
+print(np.where(df['Writing_Score']<30))
+fig,ax=plt.subplots(figsize=(18,10))
+ax.scatter(df['Placement_Score'],df['Placement_Offer_Count'])
+plt.show()
+print(np.where((df['Placement_Score']<50)))
+print(np.where((df['Placement_Score']>85)))
+z=np.abs(stats.zscore(df['Math_Score']))
+print(z)
+threshold=0.18
+sample_outliers=np.where(z<threshold)
+print(sample_outliers)
+
+
+#trimming and removing outlier
+for i in sample_outliers:
+    df.drop(i,inplace=True)
+df
+
+#change the scale
+#decreasing skewness
+
+df['Math_Score'].plot(kind='hist')
+df['log_math']=np.log10(df['Math_Score'])
+df['log_math'].plot(kind='hist')
